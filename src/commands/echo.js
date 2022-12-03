109c9d6e314e2ed18c9be5bdcb7e32534b6f549b
mallusrgreatv2 <69511006+mallusrgreatv2@users.noreply.github.com>
2022-12-03 11:27:27 +0530

    Update echo.js
    Instead of the bot sending back only one word, it now sends back entire sentences

--- src/commands/echo.js
+++ src/commands/echo.js
@@ -3,6 +3,6 @@ module.exports = {
     run(bot, message, args) {
         // `args` is an Array which contains everything after the command
         if(!args[0]) return bot.sendMessage(message.chat.id, "Put some text after /echo to make me echo something!")
-        bot.sendMessage(message.chat.id, args[0]);
+        bot.sendMessage(message.chat.id, args.join(' '));
     }   
 }
